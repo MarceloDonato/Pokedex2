f70963ec81a69b0372528bd461bdfa10f8707c3d
marceloDonato <62271815+MarceloDonato@users.noreply.github.com>
2021-07-15 01:59:10 -0300

    correção OnboardingActivity

--- app/src/main/java/com/marcelo/pokedex2/onboarding/OnboardingActitivy.kt
+++ app/src/main/java/com/marcelo/pokedex2/onboarding/OnboardingActitivy.kt
@@ -1,11 +1,28 @@
 package com.marcelo.pokedex2.onboarding
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcelo.pokedex2.R
+import com.marcelo.pokedex2.view.MainActivity
+import kotlinx.android.synthetic.main.activity_onboarding_actitivy.*
 
 class OnboardingActitivy : AppCompatActivity() {
+
+    companion object {
+        fun getStartIntent(context: Context) = Intent(context, OnboardingActitivy::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_actitivy)
+
+        start_home_button.setOnClickListener{
+            startActivity(MainActivity.getStartIntent(this))
+            finish()
+        }
+
     }
+
 }
--- app/src/main/java/com/marcelo/pokedex2/splash/SplashActivity.kt
+++ app/src/main/java/com/marcelo/pokedex2/splash/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.marcelo.pokedex2.R
-import com.marcelo.pokedex2.onboarding.OnboardingActivity
+import com.marcelo.pokedex2.onboarding.OnboardingActitivy
 import com.marcelo.pokedex2.view.MainActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -31,7 +31,8 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun startWelcomeScreen(){
-        startActivity(MainActivity.getStartIntent(this))
+
+        startActivity(OnboardingActitivy.getStartIntent(this))
         finish()
 
     }
